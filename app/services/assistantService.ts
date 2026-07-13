b5c3e43498ab1eac8195e206aa9cbe11cdae81ea
JOUDASHY <alitsiryeddynilsen@gmail.com>
2026-07-13 12:03:28 +0300

    a

--- app/services/assistantService.ts
+++ app/services/assistantService.ts
@@ -3,7 +3,7 @@ import { apiNoAuth } from "../lib/axiosClient";
 export const AssistantService = {
   poserQuestion: async (question: string): Promise<string> => {
     try {
-      const response = await apiNoAuth.post("/chat/", { question });
+      const response = await apiNoAuth.post("/rag/chat/", { question });
       return response.data.reponse;
     } catch (error) {
       console.error("Erreur lors de la communication avec l'assistant", error);
